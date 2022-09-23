6887e64529ad8111ea43b3a44e7d73e53974d68a
OdenShirataki <git config user.email>
2022-09-23 18:02:03 +0900

    change "IdSet" to "RowSet"

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ use avltriee::{
 
 pub use avltriee::RemoveResult;
 
-pub type IdSet = HashSet<u32,BuildHasherDefault<FxHasher>>;
+pub type RowSet = HashSet<u32,BuildHasherDefault<FxHasher>>;
 
 pub struct IdxSized<T>{
     mmap:FileMmap
@@ -40,27 +40,27 @@ impl<T: std::default::Default + Copy> IdxSized<T>{
     pub fn triee(&self)->&AVLTriee<T>{
         &self.triee
     }
-    pub fn value(&self,id:u32)->Option<T>{
-        self.triee.entity_value(id).map(|v|*v)
+    pub fn value(&self,row:u32)->Option<T>{
+        self.triee.entity_value(row).map(|v|*v)
     }
     pub fn insert(&mut self,target:T)->Option<u32> where T:Default + std::cmp::Ord{
         if self.triee.root()==0{ //データがまだ無い場合は新規登録
             self.init(target,1)
         }else{
-            let (ord,found_id)=self.triee.search(&target);
-            assert_ne!(0,found_id);
+            let (ord,found_row)=self.triee.search(&target);
+            assert_ne!(0,found_row);
             if ord==Ordering::Equal{
-                self.insert_same(found_id,0)
+                self.insert_same(found_row,0)
             }else{
-                self.insert_unique(target,found_id,ord,0)
+                self.insert_unique(target,found_row,ord,0)
             }
         }
     }
-    pub fn update(&mut self,id:u32,value:T) where T:std::cmp::Ord{
-        self.triee.update(id,value);
+    pub fn update(&mut self,row:u32,value:T) where T:std::cmp::Ord{
+        self.triee.update(row,value);
     }
-    pub fn delete(&mut self,id:u32)->RemoveResult<T>{
-        self.triee.remove(id)
+    pub fn delete(&mut self,row:u32)->RemoveResult<T>{
+        self.triee.remove(row)
     }
     pub fn resize_to(&mut self,record_count:u32)->Result<u32,std::io::Error>{
         let size=mem::size_of::<usize>()
@@ -71,15 +71,15 @@ impl<T: std::default::Default + Copy> IdxSized<T>{
         }
         Ok(record_count)
     }
-    pub fn max_id(&self)->u32{
+    pub fn max_rows(&self)->u32{
         let len=self.mmap.len();
         
         ((len-INIT_SIZE)/mem::size_of::<AVLTrieeNode<T>>() as u64) as u32
     }
-    fn resize(&mut self,insert_id:u32)->Result<u32,std::io::Error>{
-        let new_record_count=self.max_id();
-        let sizing_count=if insert_id!=0{
-            insert_id
+    fn resize(&mut self,insert_row:u32)->Result<u32,std::io::Error>{
+        let new_record_count=self.max_rows();
+        let sizing_count=if insert_row!=0{
+            insert_row
         }else{
             new_record_count
         };
@@ -107,46 +107,46 @@ impl<T: std::default::Default + Copy> IdxSized<T>{
         ,data:T
         ,root: u32   //起点ノード（親ノード）
         ,ord: Ordering
-        ,insert_id:u32
+        ,insert_row:u32
     )->Option<u32> where T:Default{
         if root==0{    //初回登録
-            self.init(data,insert_id)
+            self.init(data,insert_row)
         }else{
-            match self.resize(insert_id){
+            match self.resize(insert_row){
                 Err(_)=>None
-                ,Ok(new_id)=>{
-                    self.triee.update_node(root,new_id,data,ord);
-                    Some(new_id)
+                ,Ok(new_row)=>{
+                    self.triee.update_node(root,new_row,data,ord);
+                    Some(new_row)
                  }
              }
          }
     }
-    pub fn insert_same(&mut self,root:u32,insert_id:u32)->Option<u32>{
-        match self.resize(insert_id){
+    pub fn insert_same(&mut self,root:u32,insert_row:u32)->Option<u32>{
+        match self.resize(insert_row){
             Err(_)=>None
-            ,Ok(new_id)=>{
-                self.triee.update_same(root,new_id);
-                Some(new_id)
+            ,Ok(new_row)=>{
+                self.triee.update_same(root,new_row);
+                Some(new_row)
             }
         }
     }
 
-    pub fn select_by_value_from_to(&self,value_min:&T,value_max:&T)->IdSet where T:std::cmp::Ord{
-        let mut result=IdSet::default();
+    pub fn select_by_value_from_to(&self,value_min:&T,value_max:&T)->RowSet where T:std::cmp::Ord{
+        let mut result=RowSet::default();
         for (_,i,_) in self.triee().iter_by_value_from_to(value_min,value_max){
             result.insert(i);
         }
         result
     }
-    pub fn select_by_value_from(&self,value_min:&T)->IdSet where T:std::cmp::Ord{
-        let mut result=IdSet::default();
+    pub fn select_by_value_from(&self,value_min:&T)->RowSet where T:std::cmp::Ord{
+        let mut result=RowSet::default();
         for (_,i,_) in self.triee().iter_by_value_from(value_min){
             result.insert(i);
         }
         result
     }
-    pub fn select_by_value_to(&self,value_max:&T)->IdSet where T:std::cmp::Ord{
-        let mut result=IdSet::default();
+    pub fn select_by_value_to(&self,value_max:&T)->RowSet where T:std::cmp::Ord{
+        let mut result=RowSet::default();
         for (_,i,_) in self.triee().iter_by_value_to(value_max){
             result.insert(i);
         }
