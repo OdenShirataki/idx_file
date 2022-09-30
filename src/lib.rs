235ab361cd9641e85e7f2db46981dbd0a73754cc
OdenShirataki <git config user.email>
2022-09-30 18:24:22 +0900

    range iter is sames already include

--- src/lib.rs
+++ src/lib.rs
@@ -137,37 +137,22 @@ impl<T: std::default::Default + Copy> IdxSized<T>{
     }
     pub fn select_by_value_from_to(&self,value_min:&T,value_max:&T)->RowSet where T:std::cmp::Ord{
         let mut result=RowSet::default();
-        let mut last=0;
         for (_,i,_) in self.triee().iter_by_value_from_to(value_min,value_max){
             result.insert(i);
-            last=i;
-        }
-        if last>0{
-            result.append(&mut self.triee().sames(last).iter().map(|&x|x).collect());
         }
         result
     }
     pub fn select_by_value_from(&self,value_min:&T)->RowSet where T:std::cmp::Ord{
         let mut result=RowSet::default();
-        let mut last=0;
         for (_,i,_) in self.triee().iter_by_value_from(value_min){
             result.insert(i);
-            last=i;
-        }
-        if last>0{
-            result.append(&mut self.triee().sames(last).iter().map(|&x|x).collect());
         }
         result
     }
     pub fn select_by_value_to(&self,value_max:&T)->RowSet where T:std::cmp::Ord{
         let mut result=RowSet::default();
-        let mut last=0;
         for (_,i,_) in self.triee().iter_by_value_to(value_max){
             result.insert(i);
-            last=i;
-        }
-        if last>0{
-            result.append(&mut self.triee().sames(last).iter().map(|&x|x).collect());
         }
         result
     }
