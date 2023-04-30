567c2d8f5fc9752fdee0a5e1c460f87b8f313574
Oden <oden.srtk@gmail.com>
2023-05-01 08:32:55 +0900

    refactor!

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # idx_sized
+## Features
+This is a library for handling single-dimensional array data. It uses mmap and avltriee.
+
+The data that can be handled is limited to fixed-length data.
+(If you're dealing with variable length data, do better with generics.)
+
+Array data is a balanced tree algorithm that iterates from the minimum value to the maximum value, but the inserted value is always added to the end of the file and stays in the same position all the time.
+In other words, sorting, searching, and obtaining values ​​by specifying rows can all be processed at high speed.
+Also, since I'm using mmap, when I update the value it's automatically saved to the file.
+
+## Usage
+### init
+```rust
+use idx_sized::IdxSized;
+
+let mut idx=IdxSized::<i64>::new("hoge.idx").unwrap();
+```
+
+### insert
+```rust
+idx.insert(100).unwrap();
+idx.insert(300).unwrap();
+idx.insert(100).unwrap();
+idx.insert(150).unwrap();
+```
+
+### update
+```rust
+idx.update(2, 50).unwrap();
+```
+
+### delete
+```rust
+idx.delete(1).unwrap();
+```
+
+### search
+```rust
+for i in idx.triee().iter() {
+    println!("{}. {} : {}", i.index(), i.row(), i.value());
+}
+
+for row in idx.triee().iter_by_value(&100) {
+    println!("{}. {} : {}", row.index(),row.row(), row.value());
+}
+for row in idx.triee().iter_by_value_from(&100) {
+    println!("{}. {} : {}", row.index(),row.row(), row.value());
+}
+for row in idx.triee().iter_by_value_to(&200) {
+    println!("{}. {} : {}", row.index(),row.row(), row.value());
+}
+for row in idx.triee().iter_by_value_from_to(&100, &200) {
+    println!("{}. {} : {}", row.index(),row.row(), row.value());
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,23 @@
-use avltriee::AvltrieeNode;
-pub use avltriee::{Avltriee, AvltrieeIter, Removed};
-use file_mmap::FileMmap;
-use std::{cmp::Ordering, collections::BTreeSet, io, mem::size_of, path::Path};
+use std::{cmp::Ordering, io, mem::size_of, path::Path};
 
-pub type RowSet = BTreeSet<u32>;
+pub use anyhow;
+use anyhow::Result;
+
+use avltriee::AvltrieeNode;
+use avltriee::Removed as AvltrieeRemoved;
+pub use avltriee::{Avltriee, AvltrieeIter, Found};
+use file_mmap::FileMmap;
+
+pub enum Removed<T> {
+    Last(T),
+    Remain,
+    None,
+}
 
 pub struct IdxSized<T> {
     mmap: FileMmap,
     triee: Avltriee<T>,
 }
-
 impl<T> IdxSized<T> {
     const UNIT_SIZE: u64 = size_of::<AvltrieeNode<T>>() as u64;
     pub fn new<P: AsRef<Path>>(path: P) -> io::Result<Self> {
@@ -23,42 +31,49 @@ impl<T> IdxSized<T> {
             triee: Avltriee::new(p),
         })
     }
-    pub fn triee_mut(&mut self) -> &mut Avltriee<T> {
-        &mut self.triee
-    }
     pub fn triee(&self) -> &Avltriee<T> {
         &self.triee
     }
-    pub fn value(&self, row: u32) -> Option<T>
+    pub fn value(&self, row: u32) -> Option<&T>
     where
         T: Clone,
     {
         if let Ok(max_rows) = self.max_rows() {
             if row <= max_rows {
-                return unsafe { self.triee.value(row) }.map(|v| v.clone());
+                return unsafe { self.triee.value(row) };
             }
         }
         None
     }
-    pub fn insert(&mut self, target: T) -> io::Result<u32>
+
+    pub fn insert(&mut self, value: T) -> io::Result<u32>
     where
-        T: Default + Clone + Ord,
+        T: Clone + Ord,
     {
         if self.triee.root() == 0 {
-            self.init(target, 1)
+            self.init(1, value)
         } else {
-            let (ord, found_row) = self.triee.search(&target);
-            assert_ne!(0, found_row);
-            if ord == Ordering::Equal {
-                self.insert_same(found_row, 0)
+            let found = self.triee.search(&value);
+            if found.ord() == Ordering::Equal {
+                self.insert_same(found.row())
             } else {
-                self.insert_unique(target, found_row, ord, 0)
+                self.insert_unique(value, found)
             }
         }
     }
+    pub fn insert_same(&mut self, row: u32) -> io::Result<u32>
+    where
+        T: Clone,
+    {
+        self.update_same(0, row)
+    }
+    pub fn insert_unique(&mut self, value: T, found: Found) -> io::Result<u32> {
+        self.update_unique(0, value, found)
+    }
+
     pub fn update(&mut self, row: u32, value: T) -> io::Result<u32>
     where
-        T: Ord + Clone + Default,
+        T: Ord + Clone,
     {
         self.expand_to(row)?;
         unsafe {
@@ -66,13 +81,43 @@ impl<T> IdxSized<T> {
         }
         Ok(row)
     }
+    pub fn update_manually<V>(&mut self, row: u32, mut make_value: V, found: Found) -> Result<u32>
+    where
+        V: FnMut() -> Result<T>,
+        T: Clone,
+    {
+        let found_ord = found.ord();
+        let found_row = found.row();
+        if found_ord == Ordering::Equal && found_row != 0 {
+            Ok(self.update_same(row, found_row)?)
+        } else {
+            let v = make_value()?;
+            if self.exists(row) {
+                unsafe {
+                    self.triee.update_unique(row, v, found);
+                }
+                Ok(row)
+            } else {
+                Ok(self.update_unique(row, v, found)?)
+            }
+        }
+    }
+
     pub fn delete(&mut self, row: u32) -> io::Result<Removed<T>>
     where
-        T: Default + Clone,
+        T: Clone,
     {
         if let Ok(max_rows) = self.max_rows() {
             if row <= max_rows {
-                let ret = unsafe { self.triee.remove(row) };
+                let ret = {
+                    match unsafe { self.triee.remove(row) } {
+                        AvltrieeRemoved::Last => {
+                            Removed::Last(unsafe { self.triee.value_unchecked(row) }.clone())
+                        }
+                        AvltrieeRemoved::Remain => Removed::Remain,
+                        AvltrieeRemoved::None => Removed::None,
+                    }
+                };
                 if row == max_rows {
                     let mut current = row - 1;
                     if current >= 1 {
@@ -90,6 +135,47 @@ impl<T> IdxSized<T> {
         }
         Ok(Removed::None)
     }
+
+    pub fn exists(&self, row: u32) -> bool {
+        let mut exists = false;
+        if let Ok(max_rows) = self.max_rows() {
+            if row <= max_rows {
+                if let Some(_) = unsafe { self.triee.node(row) } {
+                    exists = true;
+                }
+            }
+        }
+        exists
+    }
+
+    fn update_unique(&mut self, row: u32, value: T, found: Found) -> io::Result<u32> {
+        let parent = found.row();
+        if parent == 0 {
+            self.init(if row == 0 { 1 } else { row }, value)
+        } else {
+            let new_row = self.new_row(row)?;
+            unsafe {
+                self.triee.update_unique(new_row, value, found);
+            }
+            Ok(new_row)
+        }
+    }
+    fn update_same(&mut self, row: u32, parent: u32) -> io::Result<u32>
+    where
+        T: Clone,
+    {
+        let new_row = self.new_row(row)?;
+        unsafe {
+            self.triee.update_same(new_row, parent);
+        }
+        Ok(new_row)
+    }
+
+    fn new_row(&mut self, row: u32) -> io::Result<u32> {
+        let sizing_count = if row != 0 { row } else { self.max_rows()? + 1 };
+        self.expand_to(sizing_count)
+    }
+
     fn expand_to(&mut self, record_count: u32) -> io::Result<u32> {
         let size = Self::UNIT_SIZE * (record_count + 1) as u64;
         if self.mmap.len()? < size {
@@ -98,101 +184,44 @@ impl<T> IdxSized<T> {
         }
         Ok(record_count)
     }
-    pub fn max_rows(&self) -> io::Result<u32> {
+
+    fn max_rows(&self) -> io::Result<u32> {
         Ok((self.mmap.len()? / Self::UNIT_SIZE) as u32 - 1)
     }
-    fn new_row(&mut self, insert_row: u32) -> io::Result<u32> {
-        let sizing_count = if insert_row != 0 {
-            insert_row
-        } else {
-            self.max_rows()? + 1
-        };
-        self.expand_to(sizing_count)
-    }
-    pub fn init(&mut self, data: T, root: u32) -> io::Result<u32>
-    where
-        T: Default,
-    {
+    fn init(&mut self, root: u32, data: T) -> io::Result<u32> {
         self.expand_to(root)?;
         self.triee.init_node(data, root);
         Ok(root)
     }
-    pub fn insert_unique(
-        &mut self,
-        data: T,
-        parent: u32,
-        ord: Ordering,
-        insert_row: u32,
-    ) -> io::Result<u32>
-    where
-        T: Default,
-    {
-        if parent == 0 {
-            self.init(data, if insert_row == 0 { 1 } else { insert_row })
-        } else {
-            let new_row = self.new_row(insert_row)?;
-            unsafe {
-                self.triee.update_node(parent, new_row, data, ord);
-            }
-            Ok(new_row)
-        }
-    }
-    pub fn insert_same(&mut self, parent: u32, insert_row: u32) -> io::Result<u32>
-    where
-        T: Clone,
-    {
-        let new_row = self.new_row(insert_row)?;
-        unsafe {
-            self.triee.update_same(parent, new_row);
-        }
-        Ok(new_row)
+}
+
+/*
+fn example() {
+    let mut idx = IdxSized::<i64>::new("example.idx").unwrap();
+    idx.insert(100).unwrap();
+    idx.insert(300).unwrap();
+    idx.insert(100).unwrap();
+    idx.insert(150).unwrap();
+
+    idx.update(2, 50).unwrap();
+
+    idx.delete(1).unwrap();
+
+    for i in idx.triee().iter() {
+        println!("{}. {} : {}", i.index(), i.row(), i.value());
     }
 
-    pub fn select_by_value(&self, value: &T) -> RowSet
-    where
-        T: Ord,
-    {
-        let mut result = RowSet::default();
-        let (ord, row) = self.triee().search(value);
-        if ord == Ordering::Equal && row > 0 {
-            result.insert(row);
-            result.append(
-                &mut unsafe { self.triee().sames(row) }
-                    .iter()
-                    .map(|&x| x)
-                    .collect(),
-            );
-        }
-        result
+    for row in idx.triee().iter_by_value(&100) {
+        println!("{}. {} : {}", row.index(),row.row(), row.value());
     }
-    pub fn select_by_value_from_to(&self, value_min: &T, value_max: &T) -> RowSet
-    where
-        T: Ord,
-    {
-        let mut result = RowSet::default();
-        for r in self.triee().iter_by_value_from_to(value_min, value_max) {
-            result.insert(r.row());
-        }
-        result
+    for row in idx.triee().iter_by_value_from(&100) {
+        println!("{}. {} : {}", row.index(),row.row(), row.value());
     }
-    pub fn select_by_value_from(&self, value_min: &T) -> RowSet
-    where
-        T: Ord,
-    {
-        let mut result = RowSet::default();
-        for r in self.triee().iter_by_value_from(value_min) {
-            result.insert(r.row());
-        }
-        result
+    for row in idx.triee().iter_by_value_to(&200) {
+        println!("{}. {} : {}", row.index(),row.row(), row.value());
     }
-    pub fn select_by_value_to(&self, value_max: &T) -> RowSet
-    where
-        T: Ord,
-    {
-        let mut result = RowSet::default();
-        for r in self.triee().iter_by_value_to(value_max) {
-            result.insert(r.row());
-        }
-        result
+    for row in idx.triee().iter_by_value_from_to(&100, &200) {
+        println!("{}. {} : {}", row.index(),row.row(), row.value());
     }
 }
+*/
