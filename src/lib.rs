f8193dc56cc87d5ed28e9b67cc783675dbd1d536
OdenShirataki <oden.srtk@gmail.com>
2023-08-13 12:29:17 +0900

    refactor: unwrap id::Result
    due to unrecoverable error

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
 use std::{
-    io,
     mem::size_of,
     ops::{Deref, DerefMut},
     path::Path,
 };
 
 use avltriee::AvltrieeNode;
-pub use avltriee::{anyhow, Avltriee, AvltrieeHolder, AvltrieeIter, Found};
-
-use anyhow::Result;
+pub use avltriee::{Avltriee, AvltrieeHolder, AvltrieeIter, Found};
 
 pub use file_mmap::FileMmap;
 
@@ -31,18 +28,18 @@ impl<T> DerefMut for IdxFile<T> {
 impl<T> IdxFile<T> {
     const UNIT_SIZE: u64 = size_of::<AvltrieeNode<T>>() as u64;
 
-    pub fn new<P: AsRef<Path>>(path: P) -> io::Result<Self> {
-        let mut filemmap = FileMmap::new(path)?;
-        if filemmap.len()? == 0 {
-            filemmap.set_len(Self::UNIT_SIZE)?;
+    pub fn new<P: AsRef<Path>>(path: P) -> Self {
+        let mut filemmap = FileMmap::new(path).unwrap();
+        if filemmap.len() == 0 {
+            filemmap.set_len(Self::UNIT_SIZE).unwrap();
         }
         let triee = Avltriee::new(filemmap.as_ptr() as *mut AvltrieeNode<T>);
-        let max_rows = Self::calc_max_rows(filemmap.len()?);
-        Ok(IdxFile {
+        let max_rows = Self::calc_max_rows(filemmap.len());
+        IdxFile {
             mmap: filemmap,
             triee,
             max_rows,
-        })
+        }
     }
 
     pub fn value(&self, row: u32) -> Option<&T> {
@@ -53,26 +50,26 @@ impl<T> IdxFile<T> {
         }
     }
 
-    pub fn new_row(&mut self, row: u32) -> io::Result<u32> {
+    pub fn new_row(&mut self, row: u32) -> u32 {
         self.expand_to(if row != 0 { row } else { self.max_rows + 1 })
     }
 
-    pub fn insert(&mut self, value: T) -> Result<u32>
+    pub fn insert(&mut self, value: T) -> u32
     where
         T: Ord + Clone,
     {
         self.update(0, value)
     }
-    pub fn update(&mut self, row: u32, value: T) -> Result<u32>
+    pub fn update(&mut self, row: u32, value: T) -> u32
     where
         T: Ord + Clone,
     {
-        let row = self.new_row(row)?;
-        unsafe { self.triee.update(row, value)? }
-        Ok(row)
+        let row = self.new_row(row);
+        unsafe { self.triee.update(row, value).unwrap() }
+        row
     }
 
-    pub fn delete(&mut self, row: u32) -> io::Result<()> {
+    pub fn delete(&mut self, row: u32) {
         if row <= self.max_rows {
             unsafe { self.triee.delete(row) };
             if row == self.max_rows {
@@ -85,10 +82,9 @@ impl<T> IdxFile<T> {
                         }
                     }
                 }
-                self.resize_to(Self::UNIT_SIZE * (current + 1) as u64)?;
+                self.resize_to(Self::UNIT_SIZE * (current + 1) as u64);
             }
         }
-        Ok(())
     }
 
     pub fn exists(&self, row: u32) -> bool {
@@ -101,19 +97,18 @@ impl<T> IdxFile<T> {
         exists
     }
 
-    fn expand_to(&mut self, record_count: u32) -> io::Result<u32> {
+    fn expand_to(&mut self, record_count: u32) -> u32 {
         let size = Self::UNIT_SIZE * (record_count + 1) as u64;
-        if self.mmap.len()? < size {
-            self.resize_to(size)?;
+        if self.mmap.len() < size {
+            self.resize_to(size);
         }
-        Ok(record_count)
+        record_count
     }
 
-    fn resize_to(&mut self, size: u64) -> io::Result<()> {
-        self.mmap.set_len(size)?;
+    fn resize_to(&mut self, size: u64) {
+        self.mmap.set_len(size).unwrap();
         self.triee = Avltriee::new(self.mmap.as_ptr() as *mut AvltrieeNode<T>);
         self.max_rows = Self::calc_max_rows(size);
-        Ok(())
     }
 
     fn calc_max_rows(file_len: u64) -> u32 {
